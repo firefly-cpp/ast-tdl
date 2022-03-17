c5cfe97bdb45d107176e7c2bdc143b8cb63dffbd
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-03-17 22:43:17 +0100

    Init project

---
+++ lib/ast/fdl.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "fdl/version"
+
+module Ast
+  module Fdl
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ast/fdl/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Ast
+  module Fdl
+    VERSION = "0.1.0"
+  end
+end
